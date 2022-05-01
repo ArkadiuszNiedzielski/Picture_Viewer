637d9fb134acc78f64e5c91c22c9f16ff0dec773
Kutafilec <a.niedzielski94@wp.pl>
2022-05-01 22:11:35 +0200

    GUI done

--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Picture_Viewer
 {
-    public partial class Form1 : Form
+    public partial class Form : System.Windows.Forms.Form
     {
-        public Form1()
+        public Form()
         {
             InitializeComponent();
         }
@@ -28,6 +28,11 @@ namespace Picture_Viewer
         }
 
         private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }
