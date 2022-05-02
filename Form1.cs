e7af6ae18605032d5885fc800130174ac3477f4f
Kutafilec <a.niedzielski94@wp.pl>
2022-05-02 09:49:53 +0200

    Add event handler to clear button and background button

--- Form1.cs
+++ Form1.cs
@@ -19,12 +19,16 @@ namespace Picture_Viewer
 
         private void clearButton_Click(object sender, EventArgs e)
         {
-
+            // Clear the picture.
+            pictureBox.Image = null;
         }
 
         private void backgroundButton_Click(object sender, EventArgs e)
         {
-
+            // Show the color dialog box. If the user clicks OK, change the
+            // PictureBox control's background to the color the user chose.
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox.BackColor = colorDialog1.Color;
         }
 
         private void closeButton_Click(object sender, EventArgs e)
